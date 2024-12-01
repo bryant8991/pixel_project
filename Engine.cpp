2271c90ba6f15fd3ee59ff34458edc5241b32532
Your Name <you@example.com>
2024-12-01 11:31:43 -0800

    Version 3 from ubuntu

--- Engine.cpp
+++ Engine.cpp
@@ -22,26 +22,50 @@ void Engine::run()
 	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
 	p.unitTests();
 	cout << "Unit tests complete.  Starting engine..." << endl;
-	
+
 	//Game loop
 	while (m_Window.isOpen())
 	{
-		/*
-		****************************************
-		Handle the players input
-		****************************************
-		*/
+	    // restarts clokc and returens time as a float of seconds
+	    Time time2 = clock.restart();
+	    float dt = time2.asSeconds();
 
-		Event event;
+	   // call private member functions
+	    input();
+	    update(dt);
+	    draw();
+		}
+	}
+}
+void Engine::input()
+{
+	Event event;
 		while (window.pollEvent(event))
 		{
 		    if (event.type == Event::Closed)
 		    {
-		    	// Quit the game when the window is closed
+			// Quit the game when the window is closed
 			window.close();
 		    }
 
-		    Time time2 = clock.restart();
-		    float dt = time2.asSeconds();
-		}
+		    if (event.type == sf::Event::MouseButtonPressed)
+		    {
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+			    std::cout << "the left button was pressed" << std::endl;
+			    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+			    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+			    
+			    for(int i  = 0; i < 5; i++)
+				{
+					vector<Vector2i> location;
+					location.push_back(Vector2i(event.mouseButton.x, event.mouseButton.y));
+
+					int x = rand() % 51 + 25;
+
+					Particle spot( ,x, location);
+					m_particle.push_back(spot);
+				}
+			}
+		   }
 }
